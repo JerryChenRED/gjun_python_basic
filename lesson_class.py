16baeae8945914d9fcce13fa5dea601a29450669
JerryChenRED <jerry.chen0127@gmail.com>
2023-07-04 19:11:27 +0800

    [refactor] autopep8 format

--- lesson_class.py
+++ lesson_class.py
@@ -1,14 +1,15 @@
 import abc
 
+
 class Poultry(abc.ABC):
     def __init__(self, color, sound, specy):
         self.color = color
         self.sound = sound
         self.specy = specy
-        
+
     def make_some_noise(self):
         print(f"{self.specy} is making some noise !! {self.sound * 3}")
-    
+
     def move(self):
         print(f"{self.specy}Duck move.")
 
@@ -19,18 +20,20 @@ class Poultry(abc.ABC):
     def hatch(self):
         print(f"{self.specy} hatch zzz.")
 
+
 class Duck(Poultry):
     def __init__(self, color, sound):
-        super().__init__(color, sound, 'Duck')
+        super().__init__(color, sound, "Duck")
 
     def eat(self):
         print(f"{self.specy} eat vegetable")
-    
+
     def swim(self):
         print(f"{self.specy} swim ~")
 
     def move(self):
         print(f"{self.specy} don't want to move")
+
 
 class Chicken(Poultry):
     def __init__(self, color, sound):
@@ -38,21 +41,23 @@ class Chicken(Poultry):
 
     def eat(self):
         print(f"{self.specy} eat bugs ")
-    
+
     def morning_call(self, time):
         print(f"{self.sound *3}, It's {time} a.m. now")
+
 
 class Goose(Poultry):
     def __init__(self, color, sound):
         super().__init__(color, sound, "Goose")
-    
+
     def eat(self):
         print(f"{self.specy} eat vegetable and popcorn.")
 
-if __name__ == '__main__':
-    duck_1 = Duck('yellow', "ba")
-    duck_2 = Duck('black', "ga")
-    duck_3 = Duck('white', "gua")
+
+if __name__ == "__main__":
+    duck_1 = Duck("yellow", "ba")
+    duck_2 = Duck("black", "ga")
+    duck_3 = Duck("white", "gua")
     duck_2.move()
 
     chicken_1 = Chicken("Brown", "gu")
